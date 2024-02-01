a15f77b9d38964cd999a6cbcede59b10a497968e
thegreytangent <thegreytangent@gmail.com>
2024-02-01 14:11:09 +0800

    Optimize php read

--- add-student.php
+++ add-student.php
@@ -1,15 +1,22 @@
 <?php 
 
+//get the data from the form
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 
+//------------------------- INSERT DATA TO DB --------------------------
 
+
+//1 . Make a connection to your database
 $conn = new mysqli("localhost", "dev", "devs", "student_db");
 
+// SQL STATEMENT -> Programming of lang. of database
 $sql = "INSERT INTO students (firstname, lastname) VALUES ('".$firstname."', '".$lastname."')";
 
+// is to run the sql code in php
 $conn->query($sql);
 
+//redirect
 header('location: index.php');
 
 
--- index.php
+++ index.php
@@ -1,18 +1,28 @@
+
+
+
+
+
+
+
+
+
 <!doctype html>
 <html lang="en">
 
 <head>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+   <title>Student Information</title>
 
+ 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
 
 <body>
-
     <div class="container">
     <form action="add-student.php" method="POST">
         <div class="row">
@@ -36,9 +46,41 @@
            
         </div>
         </form>
-       
+    
 
-        <div class="row">
+        <?php  
+        //create database connection
+        $conn = new mysqli("localhost", "dev", "devs", "student_db"); 
+
+        //construct sql state
+        $sql = 'SELECT * FROM students';
+        
+        $result = $conn->query($sql);
+         
+         ?>
+
+<div class="row">
+        <table class="table table-border">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+    </tr>
+  </thead>
+  <tbody>
+
+   <?php while($row = $result->fetch_assoc()): ?>
+     <tr>
+      <td><?php echo $row['id'];  ?></td>
+      <td><?php echo $row['firstname'];  ?></td>
+      <td><?php echo $row['lastname'];  ?></td>
+    </tr>
+
+    <?php endwhile; ?>
+  </tbody>
+</table>
+
 
         </div>
 
